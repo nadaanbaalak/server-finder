ca848d9323bab073496c4eef183ab37e1e7edcdd
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-05-08 03:09:20 +0530

    Added more comments

--- server-finder.js
+++ server-finder.js
@@ -10,13 +10,13 @@ function findServer(servers) {
   );
 
   return new Promise((resolve, reject) => {
-    //Creating a Promise object that either with online server with lowest priority or
+    //Creating a Promise object that either with online server with lowest priority or rejects with "No server online"
     axios
-      .all(httpRequests)
+      .all(httpRequests) //making simultaneous requests
       .then(
         axios.spread((...args) => {
-          let serverIndex = -1;
-          let serverPriority;
+          let serverIndex = -1; //index of the resolved server
+          let serverPriority; //priority of the resolved server
           args.map((arg, index) => {
             if (arg != null) {
               if (arg.status >= 200 && arg.status <= 299) {
@@ -31,14 +31,17 @@ function findServer(servers) {
             }
           });
 
+          //checking if server are online
           if (serverPriority != null && serverPriority != undefined) {
-            resolve(servers[serverIndex].url);
+            resolve(servers[serverIndex].url); //resolving with lowest priority server
           } else {
-            reject("No Server Online");
+            reject("No Server Online"); //rejecting incase of no online server
           }
         })
       )
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        throw error;
+      });
   });
 }
 
