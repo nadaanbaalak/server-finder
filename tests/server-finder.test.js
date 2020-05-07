9093b1b89124ca650f768e169f0953c8036c03a9
nadaanbaalak <abhi.sharma2800015@gmail.com>
2020-05-08 04:17:34 +0530

    Made correction in axios get mocked method

--- tests/server-finder.test.js
+++ tests/server-finder.test.js
@@ -4,73 +4,14 @@ const axios = require("axios");
 jest.mock("axios"); //mocking the axios module
 
 describe("findServer", () => {
-  // case 1: few server are offline,few servers are online and http://google.com has lowest priority
-  it("resolves with http://google.com", () => {
-    axios.get.mockResolvedValue([null, { status: 201 }, null, { status: 200 }]);
-    axios.all.mockResolvedValue([null, { status: 201 }, null, { status: 200 }]);
-    const servers = [
-      {
-        url: "http://doesNotExist.boldtech.co",
-        priority: 1,
-      },
-      {
-        url: "http://boldtech.co",
-        priority: 7,
-      },
-      {
-        url: "http://offline.boldtech.co",
-        priority: 2,
-      },
-      {
-        url: "http://google.com",
-        priority: 4,
-      },
-    ];
-    findServer(servers).then((res) => {
-      expect(res).toBe("http://google.com");
-    });
-  });
-
-  //case 2:few servers are offline,few servers are online and http://boldtech.co has lowest priority
-  it("resolves with http://boldtech.co", () => {
-    axios.get.mockResolvedValue([null, { status: 201 }, null, { status: 200 }]);
-    axios.all.mockResolvedValue([null, { status: 201 }, null, { status: 200 }]);
-    const servers = [
-      {
-        url: "http://doesNotExist.boldtech.co",
-        priority: 1,
-      },
-      {
-        url: "http://boldtech.co",
-        priority: 3,
-      },
-      {
-        url: "http://offline.boldtech.co",
-        priority: 2,
-      },
-      {
-        url: "http://google.com",
-        priority: 4,
-      },
-    ];
-    findServer(servers).then((res) => {
-      expect(res).toBe("http://boldtech.co");
-    });
-  });
-
-  // case 3: all servers are online and http://google.com has lowest priority
+  // case 1: all servers are online and http://doesNotExist.boldtech.co has lowest priority
   it("resolves with http://doesNotExist.boldtech.co", () => {
-    axios.get.mockResolvedValue([
-      { status: 201 },
-      { status: 201 },
-      { status: 204 },
-      { status: 200 },
-    ]);
+    axios.get.mockResolvedValue({ status: 201 });
     axios.all.mockResolvedValue([
       { status: 201 },
       { status: 201 },
-      { status: 204 },
-      { status: 200 },
+      { status: 201 },
+      { status: 201 },
     ]);
     const servers = [
       {
@@ -95,10 +36,47 @@ describe("findServer", () => {
     });
   });
 
-  //case 4: few server are offline with status >=300
+  // case 2: all servers are online and http://google.com has lowest priority
+  it("resolves with http://google.com", () => {
+    axios.get.mockResolvedValue({ status: 201 });
+    axios.all.mockResolvedValue([
+      { status: 201 },
+      { status: 201 },
+      { status: 201 },
+      { status: 201 },
+    ]);
+    const servers = [
+      {
+        url: "http://doesNotExist.boldtech.co",
+        priority: 4,
+      },
+      {
+        url: "http://boldtech.co",
+        priority: 7,
+      },
+      {
+        url: "http://offline.boldtech.co",
+        priority: 2,
+      },
+      {
+        url: "http://google.com",
+        priority: 1,
+      },
+    ];
+    findServer(servers).then((res) => {
+      expect(res).toBe("google.com");
+    });
+  });
+
+  //case 2: all server are offline with status >=300
   it("rejects with No Server Online", () => {
-    axios.get.mockResolvedValue([null, { status: 300 }, null, { status: 404 }]);
-    axios.all.mockResolvedValue([null, { status: 300 }, null, { status: 404 }]);
+    axios.get.mockResolvedValue({ status: 300 });
+    axios.all.mockResolvedValue([
+      { status: 300 },
+      { status: 300 },
+      { status: 300 },
+      { status: 300 },
+    ]);
     const servers = [
       {
         url: "http://doesNotExist.boldtech.co",
@@ -124,9 +102,9 @@ describe("findServer", () => {
     });
   });
 
-  // case 5: all servers are offline
+  // case 3: all servers are offline
   it("rejects with No Server Online", () => {
-    axios.get.mockResolvedValue([null, null, null, null]);
+    axios.get.mockResolvedValue(null);
     axios.all.mockResolvedValue([null, null, null, null]);
     const servers = [
       {
